c31c3fd63f6f1780f7d9d26418767f18720a09b1
workout <loves1543@naver.com>
2022-03-14 15:28:07 +0900

    스테이지 이동 카메라 등
    1

--- Assets/Script/TransferMap.cs
+++ Assets/Script/TransferMap.cs
@@ -6,11 +6,20 @@ using UnityEngine.SceneManagement; //Scene 매니저 라이브러리 추가
 public class TransferMap : MonoBehaviour
 {
     public string transferMapName; // 이동할 맵이름
+
+
+    public Transform target;
+    public BoxCollider2D targetBound;
+
     private PlayerAction thePlayer;
+    private MainCamera theCamera;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        theCamera = FindObjectOfType<MainCamera>();
         thePlayer = FindObjectOfType<PlayerAction>();
     }
 
@@ -21,7 +30,10 @@ public class TransferMap : MonoBehaviour
         if (collision.gameObject.name == "Player")
         {
             thePlayer.currentMapName = transferMapName;
-            SceneManager.LoadScene(transferMapName);
+            theCamera.SetBound(targetBound);
+            // SceneManager.LoadScene(transferMapName); 씬이동
+            theCamera.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, theCamera.transform.position.z);
+            thePlayer.transform.position = target.transform.position;
         }
     }    
 }
