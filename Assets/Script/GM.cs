d8388c5e79048fd5a4399f8f49b7580da899cd51
bousbaert <81398779+bousbaert@users.noreply.github.com>
2022-04-25 20:35:44 +0900

    Revert "Merge pull request #47 from bousbaert/YEJIN_HA"
    This reverts commit c6e3c0328cbb2340ad6f4071c34fd2517486e5f3, reversing
    changes made to 4f0895e977c70b1be399d38fe4c343f80d793957.

---
+++ Assets/Script/GM.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GM : MonoBehaviour
+{
+    public TalkManager talkManager;
+    public QuestManager questManager;
+    public Text talkText;
+    public GameObject scanObject;
+    public GameObject talkPanel;
+    public bool isAciton;
+    public int talkIndex;
+
+
+    void Start()
+    {
+        Debug.Log(questManager.CheckQuest());
+    }
+    public void Action(GameObject scanObj)
+    {
+        scanObject = scanObj;
+        ObjData objData = scanObject.GetComponent<ObjData>();
+        Talk(objData.id, objData.isNpc);
+
+        talkPanel.SetActive(isAciton);
+
+    }
+    void Talk(int id, bool isNpc)
+    {
+
+        int questTalkIndex = questManager.GetQuestTalkIndex(id);
+        string talkData = talkManager.GetTalk(id + questTalkIndex, talkIndex);
+
+        if (talkData == null)
+        {
+            isAciton = false;
+            talkIndex = 0;
+            Debug.Log(questManager.CheckQuest(id));
+            return;
+        }
+        if (isNpc)
+        {
+            talkText.text = talkData;
+        }
+        else
+        {
+            talkText.text = talkData;
+        }
+        isAciton = true;
+        talkIndex++;
+    }
+}
